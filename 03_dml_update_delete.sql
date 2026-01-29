0c2316f38fa1e5ad857c71b5a7692a3c9e2cd945
Cleverson Moura Andrade <36683447+Cleverson-Andrade@users.noreply.github.com>
2026-01-29 10:20:32 -0300

    Restore and updatrefactor: tornar executável o script 03_dml_update_delete com junções SQLe SQL table creation and joins

--- 03_dml_update_delete.sql
+++ 03_dml_update_delete.sql
@@ -1,6 +1,5 @@
---  Junções
+-- Junções
 
-/*
 CREATE TABLE profissoes(
 	id SERIAL PRIMARY KEY,
 	cargo VARCHAR(60) NOT NULL
@@ -15,92 +14,89 @@ CREATE TABLE clientes(
 );
 
 CREATE TABLE consumidor(
-   id SERIAL PRIMARY KEY,
-    nome VARCHAR(50) NOT NULL,
-    contato VARCHAR(50) NOT NULL,
-    endereco VARCHAR(100) NOT NULL,
-    cidade VARCHAR(100) NOT NULL,
-    cep VARCHAR(20) NOT NULL,
-    pais VARCHAR(50) NOT NULL
- );
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL,
+	contato VARCHAR(50) NOT NULL,
+	endereco VARCHAR(100) NOT NULL,
+	cidade VARCHAR(100) NOT NULL,
+	cep VARCHAR(20) NOT NULL,
+	pais VARCHAR(50) NOT NULL
+);
 
 INSERT INTO profissoes (cargo) VALUES ('Analista de Sistemas');
 INSERT INTO profissoes (cargo) VALUES ('Suporte');
 INSERT INTO profissoes (cargo) VALUES ('Gerente');
 
-INSERT INTO clientes (nome, data_nascimento, telefone, id_profissao) VALUES ('João Pereira', '1981-06-15', '1234-5688', 1);
-INSERT INTO clientes (nome, data_nascimento, telefone, id_profissao) VALUES ('Ricardo da Silva', '1973-10-10', '2234-5669', 2);
-INSERT INTO clientes (nome, data_nascimento, telefone, id_profissao) VALUES ('Felipe Oliveira', '1987-08-01', '4234-5640', 3);
-INSERT INTO clientes (nome, data_nascimento, telefone, id_profissao) VALUES ('Mario Pirez', '1991-02-05', '5234-5621', 1);
+INSERT INTO clientes (nome, data_nascimento, telefone, id_profissao)
+VALUES ('João Pereira', '1981-06-15', '1234-5688', 1);
 
-INSERT INTO consumidor (nome, contato, endereco, cidade, cep, pais) VALUES ('Alfredo Nunes', 'Maria Nunes', 'Rua da paz, 47', 'São Paulo', '123.456-87', 'Brasil');
-INSERT INTO consumidor (nome, contato, endereco, cidade, cep, pais) VALUES ('Ana Trujillo', 'Guilherme Souza', 'Rua Dourada, 452', 'Goiania', '232.984-23', 'Brasil');
-INSERT INTO consumidor (nome, contato, endereco, cidade, cep, pais) VALUES ('Leandro Veloz', 'Pedro Siqueira', 'Rua Vazia, 72', 'São Paulo', '936.738-23', 'Brasil');
-*/
+INSERT INTO clientes (nome, data_nascimento, telefone, id_profissao)
+VALUES ('Ricardo da Silva', '1973-10-10', '2234-5669', 2);
 
--- Junção de Produto Cartesiano 
-/*
+INSERT INTO clientes (nome, data_nascimento, telefone, id_profissao)
+VALUES ('Felipe Oliveira', '1987-08-01', '4234-5640', 3);
+
+INSERT INTO clientes (nome, data_nascimento, telefone, id_profissao)
+VALUES ('Mario Pirez', '1991-02-05', '5234-5621', 1);
+
+INSERT INTO consumidor (nome, contato, endereco, cidade, cep, pais)
+VALUES ('Alfredo Nunes', 'Maria Nunes', 'Rua da paz, 47', 'São Paulo', '123.456-87', 'Brasil');
+
+INSERT INTO consumidor (nome, contato, endereco, cidade, cep, pais)
+VALUES ('Ana Trujillo', 'Guilherme Souza', 'Rua Dourada, 452', 'Goiania', '232.984-23', 'Brasil');
+
+INSERT INTO consumidor (nome, contato, endereco, cidade, cep, pais)
+VALUES ('Leandro Veloz', 'Pedro Siqueira', 'Rua Vazia, 72', 'São Paulo', '936.738-23', 'Brasil');
+
+-- Junção de Produto Cartesiano
 SELECT c.id, c.nome, c.data_nascimento, c.telefone, p.cargo
 FROM clientes AS c, profissoes AS p
 WHERE c.id_profissao = p.id;
-*/
 
 -- Inner Join
-/*
- SELECT c.id, c.nome, c.data_nascimento, c.telefone, p.cargo
- FROM clientes AS c 
- INNER JOIN profissoes AS p
- ON  c.id_profissao = p.id;
- */
+SELECT c.id, c.nome, c.data_nascimento, c.telefone, p.cargo
+FROM clientes AS c
+INNER JOIN profissoes AS p
+ON c.id_profissao = p.id;
 
 -- Left Outer Join
-/*
- SELECT * 
- FROM clientes
- LEFT OUTER JOIN profissoes
- ON clientes.id_profissao = profissoes.id;
-*/
+SELECT *
+FROM clientes
+LEFT OUTER JOIN profissoes
+ON clientes.id_profissao = profissoes.id;
 
 -- Right Outer Join
-/*
- SELECT * 
- FROM clientes
- RIGHT OUTER JOIN profissoes
- ON clientes.id_profissao = profissoes.id;
-*/
+SELECT *
+FROM clientes
+RIGHT OUTER JOIN profissoes
+ON clientes.id_profissao = profissoes.id;
 
--- Full Outer Join -- Funciona no PostgreSQL mas nao funciona no MySQL
- /*
- SELECT * 
- FROM clientes
- FULL OUTER JOIN profissoes
- ON clientes.id_profissao = profissoes.id;
-*/
+-- Full Outer Join (PostgreSQL)
+SELECT *
+FROM clientes
+FULL OUTER JOIN profissoes
+ON clientes.id_profissao = profissoes.id;
 
--- Full Outer Join (VersÃ£o MySQL)
-/*
- SELECT * FROM clientes
- LEFT OUTER JOIN profissoes
- ON clientes.id_profissao = profissoes.id
- UNION
- SELECT * FROM clientes
- RIGHT OUTER JOIN profissoes
- ON clientes.id_profissao = profissoes.id;
-*/
+-- Full Outer Join (Versão MySQL)
+SELECT *
+FROM clientes
+LEFT OUTER JOIN profissoes
+ON clientes.id_profissao = profissoes.id
+UNION
+SELECT *
+FROM clientes
+RIGHT OUTER JOIN profissoes
+ON clientes.id_profissao = profissoes.id;
 
 -- Cross Join
-/*
- SELECT c.id, c.nome, c.data_nascimento, c.telefone, p.cargo
- FROM clientes AS c
- CROSS JOIN profissoes AS p;
-*/
+SELECT c.id, c.nome, c.data_nascimento, c.telefone, p.cargo
+FROM clientes AS c
+CROSS JOIN profissoes AS p;
 
 -- Self Join
-/*
 SELECT a.nome AS Consumidor1, b.nome AS Consumidor2, a.cidade
 FROM consumidor AS a
 INNER JOIN consumidor AS b
 ON a.id <> b.id
 AND a.cidade = b.cidade
 ORDER BY a.cidade;
-*/
